74cead947c6c62d69ac10483964497878347ef59
HACO <jasonytonlinecomeandsee@gmail.com>
2023-06-15 13:20:28 +0800

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <EasyNextionLibrary.h>
+#include <WiFiEsp.h>
 #include <SCD30.h>
 
+WiFiEspClient client;
 EasyNex myNex(Serial1);
 
 int CurrentAirTemp = 25;
+int status = WL_IDLE_STATUS;
+
+char ssid[] = "K1D1-NETWORK-2.4";
+char pass[] = "12345678";
+
+char server[] = "api.thingspeak.com";
+char apiKey[] = "HGO8UBKWK0W04DO6";
+
+unsigned long lastTime = 0;
+unsigned long interval = 20000;
 
 class SCD30dataClass
 {
@@ -39,28 +51,53 @@ void setup()
   Serial.begin(115200);
   scd30.initialize();
 
+  // Serial output setup
+  Serial.begin(9600);
+
   // Setup PM2
   Serial2.begin(9600);
 
-  // Serial output setup
-  Serial.begin(9600);
+  // ESP-01S setup
+  Serial3.begin(9600);
+  WiFi.init(&Serial3);
+  while (status != WL_CONNECTED)
+  {
+    status = WiFi.begin(ssid, pass);
+  }
 }
 
 void loop()
 {
   myNex.NextionListen();
+  SCD30dataClass SCD30data = SCD30sensor();
+  String humidString = String(SCD30data.humid, 1);
+  String tempString = String(SCD30data.temp, 1);
+  String co2String = String(SCD30data.co2, 0);
+  myNex.writeStr("Humid.txt", humidString + "%");
+  myNex.writeStr("Temp.txt", tempString + "℃");
+  myNex.writeStr("CO2.txt", co2String + " PPM");
 
-  if (myNex.currentPageId == 0)
+  // String pm25String = String(PUT_DATA_HERE);
+  // myNex.writeStr("PM25.txt", pm25String + " ug/m3");
+
+  if (millis() - lastTime >= interval)
   {
-    SCD30dataClass SCD30data = SCD30sensor();
-    String humiString = String(SCD30data.humid, 1);
-    String tempString = String(SCD30data.temp, 1);
-    String co2String = String(SCD30data.co2, 0);
-    myNex.writeStr("Humi.txt", humiString + "%");
-    myNex.writeStr("Temp.txt", tempString + "℃");
-    myNex.writeStr("CO2.txt", co2String + " PPM");
-
-    // String pm25String = String(PUT_DATA_HERE);
-    // myNex.writeStr("PM25.txt", pm25String + " ug/m3");
+    if (client.connect(server, 80))
+    {
+      Serial.println("Connecting to ThingSpeak...");
+      client.print("GET /update?api_key=");
+      client.print(apiKey);
+      client.print("&field1=");
+      client.print(SCD30data.temp);
+      client.print("&field2=");
+      client.print(SCD30data.humid);
+      client.print("&field3=");
+      client.println(SCD30data.co2);
+    }
+    else
+    {
+      Serial.println("Connection to ThingSpeak failed");
+    }
+    lastTime = millis();
   }
 }
